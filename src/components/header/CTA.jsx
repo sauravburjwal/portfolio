b42879bd55cadf82ef3a155b3da4d61d268720de
Saurav Burjwal <sauravburjwal@gmail.com>
2022-09-15 20:02:17 +0530

    update-Resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
+import React from 'react';
+import { FaDownload } from 'react-icons/fa';
+import CV from '../../assets/Resume.pdf';
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Resume <FaDownload />
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
